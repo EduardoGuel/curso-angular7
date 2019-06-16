825dfebff672c471c982ee3809b66e62611d1ad3
Eduardo Guel <fba97_mty@hotmail.com>
2019-06-16 14:40:21 -0500

    usando ngfor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  users = ['pedro', 'pablo', 'hernan', 'eduardo'];
+
   constructor() { }
 
   ngOnInit() {
